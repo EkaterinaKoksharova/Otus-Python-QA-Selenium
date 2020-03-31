9e2dc57594daa39e63e7d446fb0608cc9590c77e
Sergey Zapuhlyak <me@zsergey.com>
2020-04-01 01:23:22 +0300

    added asserts to test_main_page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,6 +3,7 @@
 
 from pages.main_page import MainPage
 from pages.common import CommonItems
+from pages.search_result_page import SearchResultPage
 
 
 class TestMainPage:
@@ -12,15 +13,17 @@ class TestMainPage:
         """ Проверка наличия основных элементов на главной странице"""
 
         browser.get(CommonItems.base_url)
-        browser.find_element(*MainPage.slider)
-        browser.find_element(*MainPage.slider_footer)
-        browser.find_element(*MainPage.featured_card_1)
-        browser.find_element(*MainPage.featured_card_2)
-        browser.find_element(*MainPage.featured_card_3)
-        browser.find_element(*MainPage.featured_card_4)
+        assert len(browser.find_elements(*MainPage.slider)) == 1
+        assert len(browser.find_elements(*MainPage.slider_footer)) == 1
+        assert len(browser.find_elements(*MainPage.featured_card_1)) == 1
+        assert len(browser.find_elements(*MainPage.featured_card_2)) == 1
+        assert len(browser.find_elements(*MainPage.featured_card_3)) == 1
+        assert len(browser.find_elements(*MainPage.featured_card_4)) == 1
 
     def test_go_to_search_result(self, browser):
         """ Проверка перехода на страницу результата поиска """
 
         browser.get(CommonItems.base_url)
         CommonItems.go_to_search_result_page(browser)
+
+        assert browser.find_element(*SearchResultPage.search_input).is_displayed()
